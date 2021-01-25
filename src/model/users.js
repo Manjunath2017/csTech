798eeec177a45cba054d4af03443c786c1cecfe8
Manjunath2017 <manjunath.jadhav1231@gmail.com>
2021-01-25 22:14:42 +0530

    mongoole ORM, user model and validaiton

---
+++ src/model/users.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+//DataBase structure
+const user = mongoose.Schema({
+    name:{
+        type:String,
+        require:[true, 'Name is required!'],
+        trim:true,
+        minLength:[2, 'Length is to shot!']
+
+    },
+    email:{
+        type:String,
+        require:[true, 'Email is required!'],
+        lowercase: true,
+        unique:true
+    },
+    salary:{
+        type:Number,
+        require:true
+    },
+    designation:{
+        type:String
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    }
+});
+
+module.exports = mongoose.model('newusers', user);
+// Registering model and we can have access across all files(module.exports)
+// 'newuser' is a collection name
+// Define 'user'

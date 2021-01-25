990554734462ae51123afc3bae34358ef5e0ecec
Manjunath2017 <manjunath.jadhav1231@gmail.com>
2021-01-25 23:20:35 +0530

    validation: if email exist send message

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,8 +2,19 @@ const app = require('express')();
 const user = require('../model/users');
 
 module.exports.addUser = async (request, response) => {
-    
     const {name, email, salary, designation}=request.body;
+
+    //convert email to lowerCase and remover space if any
+    var userEmail = email.toLowerCase().trim();
+    
+    //find by email
+    const userExist = await user.findOne({ "email":userEmail });
+        
+    //check if user exist?
+    if(userExist.email === userEmail){
+        return response.send(`Please try with different email!`);
+    }
+
     //collect user detail and store in collectUserData variable
     const collectUserData = new user({ name, email, salary, designation });
 
@@ -14,5 +25,5 @@ module.exports.addUser = async (request, response) => {
     }catch(error){
         response.status(400).send(error);
     }
-    console.log(`addUser ${collectUserData}`);
+    
 }
